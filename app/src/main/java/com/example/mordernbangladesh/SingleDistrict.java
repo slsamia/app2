72a43917b1a35424d1b90db5c0b8ffa64477f965
Monirul Islam <mr.monirul.dev@gmail.com>
2025-06-26 16:02:39 +0600

    removed unused files and folder

--- app/src/main/java/com/example/mordernbangladesh/SingleDistrict.java
+++ app/src/main/java/com/example/mordernbangladesh/SingleDistrict.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Mymenshing2 extends AppCompatActivity {
+public class SingleDistrict extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_mymenshing2);
+        setContentView(R.layout.activity_single_district);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
