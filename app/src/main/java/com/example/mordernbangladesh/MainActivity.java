25cee77b45f9a79de6b497fbe4eaf6dc187020ee
Monirul Islam <mr.monirul.dev@gmail.com>
2025-06-28 20:51:48 +0600

    add drawer menu

--- app/src/main/java/com/example/mordernbangladesh/MainActivity.java
+++ app/src/main/java/com/example/mordernbangladesh/MainActivity.java
@@ -11,22 +11,30 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
     ArrayList<JsonParser.DivisionSummary> divisionItemList = new ArrayList<>();
     LinearLayout mainDivisionListLayout;
 
@@ -41,11 +49,14 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         mainDivisionListLayout = findViewById(R.id.mainDivisionListLayout);
 
         divisionItemList = JsonParser.getAllDivisionSummaries(this);
 
-        for (int i=0;i<divisionItemList.size() -1;i = i+2) {
+        for (int i = 0; i < divisionItemList.size() - 1; i = i + 2) {
             View viewItem = LayoutInflater.from(this).inflate(R.layout.card_item, mainDivisionListLayout, false);
 
             TextView lname = viewItem.findViewById(R.id.cardLeftTitle);
@@ -54,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             ImageView rimg = viewItem.findViewById(R.id.cardRightImage);
 
             JsonParser.DivisionSummary left = divisionItemList.get(i);
-            JsonParser.DivisionSummary right = divisionItemList.get(i+1);
+            JsonParser.DivisionSummary right = divisionItemList.get(i + 1);
 
             lname.setText(left.name);
             rname.setText(right.name);
@@ -92,41 +103,53 @@ public class MainActivity extends AppCompatActivity {
             mainDivisionListLayout.addView(viewItem);
         }
 
+        setUpDrawerMenu();
+    }
 
+    private void setUpDrawerMenu() {
+
+
+        drawerLayout = findViewById(R.id.main);
+        navigationView = findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                default:
+                    Toast.makeText(this, "default clicked", Toast.LENGTH_SHORT).show();
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return  true;
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        Log.v("action_menu_click", String.valueOf(id) + " == " +  String.valueOf(R.id.action_about_us));
-        if(id == R.id.action_about_us){
+        Log.v("action_menu_click", String.valueOf(id) + " == " + String.valueOf(R.id.action_about_us));
+        if (id == R.id.action_about_us) {
 
-                Intent intent = new Intent(MainActivity.this, AboutUs.class);
-                startActivity(intent);
-                return true;
+            Intent intent = new Intent(MainActivity.this, AboutUs.class);
+            startActivity(intent);
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
 
 
-class DivisionItem {
-    String lname;
-    String limage;
-    String rname;
-    String rimage;
-
-    DivisionItem(String lname, String limage, String rname, String rimage) {
-        this.lname = lname;
-        this.rname = rname;
-        this.limage = limage;
-        this.rimage = rimage;
-    }
-}
